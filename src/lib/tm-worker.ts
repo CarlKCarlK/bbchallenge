247e02d8aeb3126d409649f673589851fd37bd28
Carl Kadie <carlk@msn.com>
2025-03-20 16:19:31 +0000

    worker seems to give some intermediate results

--- src/lib/tm-worker.ts
+++ src/lib/tm-worker.ts
@@ -2,6 +2,7 @@ import init, { SpaceByTimeMachine } from './blaze/pkg/busy_beaver_blaze';
 
 self.onmessage = async (event: MessageEvent) => {
     const { machineCode, canvasWidth, canvasHeight, binning, stepCount } = event.data;
+    const run_for_seconds = 0.1;
 
     try {
         // Initialize the WASM module
@@ -17,8 +18,16 @@ self.onmessage = async (event: MessageEvent) => {
         );
 
         // Perform the computation
-        if (stepCount > 1) {
-            spaceTimeMachine.nth(BigInt(stepCount) - 2n);
+        // eslint-disable-next-line no-constant-condition
+        while (true) {
+            if (!spaceTimeMachine.step_for_secs(
+                run_for_seconds, 
+                BigInt(stepCount), // Convert stepCount to BigInt
+                10_000n // Already a BigInt
+            )) break;
+
+            const pngData = spaceTimeMachine.png_data();
+            self.postMessage({ type: 'image', data: pngData }, [pngData.buffer]); // Send the buffer with metadata
         }
 
         // Get the PNG data and send it back to the main thread
